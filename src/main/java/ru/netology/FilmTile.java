44195debb668ab062f13ea4310a8db8175f4e768
zaksignu <zaksignu@gmail.com>
2022-02-08 16:07:46 +0300

    Second commit

---
+++ src/main/java/ru/netology/FilmTile.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+public class FilmTile {
+    private int tileId;
+    private String filmName;
+    private String filmGenre;
+    private String filmPicture;
+
+    public FilmTile(int tileId, String filmName, String filmGenre, String filmPicture) {
+        this.tileId = tileId;
+        this.filmName = filmName;
+        this.filmGenre = filmGenre;
+        this.filmPicture = filmPicture;
+    }
+
+
+}
+
---
+++ src/main/java/ru/netology/PosterManager.java
@@ -0,0 +1,60 @@
+package ru.netology;
+
+public class PosterManager {
+    private int filmShowLimit;
+    private FilmTile[] tiles = new FilmTile[0];
+
+    //Простой конструктор с значением лимита выдачи по-умолчанию
+    public PosterManager() {
+        filmShowLimit = 5;
+    }
+
+    //Конструктор с выставлением требуемого лимит выдачи
+    public PosterManager(int filmShowLimit) {
+        if (filmShowLimit >= 1) {
+            this.filmShowLimit = filmShowLimit;
+        } else {
+            filmShowLimit = 5;
+        }
+    }
+
+    public int getFilmShowLimit() {
+        return this.filmShowLimit;
+    }
+
+    public void setFilmShowLimit(int filmShowLimit) {
+        this.filmShowLimit = filmShowLimit;
+    }
+
+    public void addTile(FilmTile item) {
+        int length = tiles.length + 1;
+        FilmTile[] tmp = new FilmTile[length];
+        for (int i = 0; i < length - 1; i++) {
+            tmp[i] = tiles[i];
+        }
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        tiles = tmp;
+    }
+
+    public FilmTile[] showTiles() {
+        return tiles;
+    }
+
+    public FilmTile[] showSomeTiles() {
+
+        int length = tiles.length - 1;
+        int p = 0;
+        if ((tiles.length - filmShowLimit) <=0) {
+            filmShowLimit = tiles.length;
+        }
+        FilmTile[] tmp = new FilmTile[filmShowLimit];
+        for (int i = (tiles.length - 1); i >= ((tiles.length - filmShowLimit)); i--) {
+            tmp[p] = tiles[i];
+            p++;
+        }
+        tiles = tmp;
+        return tiles;
+    }
+
+}
